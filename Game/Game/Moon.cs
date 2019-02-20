c14c7363df661d5ef60c7bc62aac222ef3e641a2
Kazantsev85 <kazantsev_85@mail.ru>
2019-02-21 02:35:17 +0300

    Домашнее задание С#2.1 rev1

---
+++ Game/Game/Moon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Moon : BaseObject
+    {
+        public Moon(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }
+        public override void Draw()
+        {
+            Image newImage = Image.FromFile("C:\\Users\\aik\\OneDrive\\Документы\\C#\\C#2_HW\\Game\\Game\\Images\\moon.jpg");
+            
+            Game.Buffer.Graphics.DrawImage(newImage, Pos.X, Pos.Y, Size.Width, Size.Height);
+
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+
+        }
+    }
+}
---
+++ Game/Game/SplashScreen.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    public partial class SplashScreen : Form
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;        
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+        
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Refrash();
+        }
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (SplashScreenObjects obj in objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+        public static void Refrash()
+        {
+            foreach (SplashScreenObjects obj in objs)
+                obj.Update();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.NewGame();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();            
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = CreateGraphics();
+
+
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Loader();
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+        static SplashScreenObjects[] objs;
+        public static void Loader()
+        {
+            objs = new SplashScreenObjects[30];
+            
+            for (int i = 0; i < objs.Length; i++)
+                objs[i] = new SplashScreenObjects(new Point(200, 200), new Point(-1, 0), new Size(5, 5));
+            
+        }
+    }
+}
