e5534449d69294ab871b01041fe59926dd74d5ff
Kazantsev85 <kazantsev_85@mail.ru>
2019-02-21 02:35:17 +0300

    Доьашнее задание С#2.1 rev1

---
+++ Game/Game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);         
+            Application.Run(new SplashScreen());    
+            
+        }
+        static public void NewGame()
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            
+        }
+    }
+}
